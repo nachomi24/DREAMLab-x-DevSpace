23d8db7168a873dd84beac5d0d8274b00d4669a9
karengtz04 <102173278+karengtz04@users.noreply.github.com>
2024-04-09 11:33:06 -0600

    Página de Garage - HU009

---
+++ HU009/src/Components/Content/Content.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Content.css'
+import Card from '../Card/Card'
+
+const Content = () => {
+  return (
+    <div className='Content container'>
+        <div className='Content-text'>
+            <h1>GARAGE VALLEY</h1>
+
+        </div>
+
+      
+    </div>
+  )
+}
+
+export default Content
