914bc8d557d9adb79f1e278fb5f73b306b922e3d
roberta gzz <a01368580@tec.mx>
2024-04-16 17:57:01 -0600

    Front de secciones

---
+++ proyectoDreamLab/src/HU008/componentes/Boton/Boton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import boton from '../../assets/boton.png'
+
+const Boton = () => {
+  return (
+    <div>
+        <img src={boton} className='botonnav'/>
+    </div>
+  )
+}
+
+export default Boton
---
+++ proyectoDreamLab/src/HU008/componentes/secciones/Secciones.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import OS from '../../assets/OS.png'
+import './Secciones.css'
+
+const Secciones = () => {
+  return (
+    <div className='box'>
+        <img src={OS} alt="Open Spaces" className='OS container'/>
+    </div>
+  )
+}
+
+export default Secciones
