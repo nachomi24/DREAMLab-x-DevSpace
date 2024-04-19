7f13372bd2a7a8d82feeba3b8f397d859340bd85
criostec <a00833125@tec.mx>
2024-04-18 21:27:25 -0600

    Cambios a profile y app de hu0019

--- proyectoDreamLab/src/HU0019/App.jsx
+++ proyectoDreamLab/src/HU0019/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Content from './Components/Content/Content'
 import Info from './Components/Info/Info'
 import Card from './Components/Card/Card'
 import Profile from './Components/Profile/Profile'
-const App = () => {
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+function App({ matricula}) {
+  const apiURLPerfil = "https://devspaceapi.azurewebsites.net/api/perfil/" + matricula;
+  const apiURLReservaciones = "https://devspaceapi.azurewebsites.net/api/consulta-reservacion/" + matricula;
+  const [carrera, setCarrera] = useState('');
+  const [puntos, setPuntos] = useState(0);
+  const [nombre, setNombre] = useState('');
+  const [ufCursando, setUfCursando] = useState(0);
+  const [reservaciones, setReservaciones] = useState([]);
+
   return (
     <div>
       <Navbar />
--- proyectoDreamLab/src/HU0019/Components/Profile/Profile.jsx
+++ proyectoDreamLab/src/HU0019/Components/Profile/Profile.jsx
@@ -1,12 +1,15 @@
 import './Profile.css'
 import karenProfile from '../../../assets/karenFoto.png'
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+
+
+
+
+
 const Profile = () => {
     return (
         <div class="image-container">
             <img class="image-container" src={karenProfile} alt="karenProfile" border="0" className='Profile container' />
-            <p class="matricula-text">A00835268</p>
+            <p class="matricula-text">{matricula}</p>
             <p class="descripcion-text">Estudiante - ITC</p>
             <p class="puntos-text">Puntos de prioridad</p>
             <p class="puntos">82</p>
