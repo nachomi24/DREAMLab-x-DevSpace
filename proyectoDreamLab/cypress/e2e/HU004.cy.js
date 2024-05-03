103224393ffeb1c9245955415831d1f8006c5410
Nahomi Plata Ulate <nahomikawaii@gmail.com>
2024-05-03 10:31:43 -0600

    Agregué pruebas faltantes

--- proyectoDreamLab/cypress/e2e/HU004.cy.js
+++ proyectoDreamLab/cypress/e2e/HU004.cy.js
@@ -4,13 +4,30 @@ describe('template spec', () => {
         cy.visit('http://localhost:5173/')
     })
 
-    it('probar que pueda ir a la seccion de reservar',()=>{
+    it('matrícula correcta',()=>{
         cy.get('.fa-bars').click()
         cy.get(':nth-child(2) > .mina-bold-2').click()
-        cy.get('#conversation_query').type('A01027008')
+        cy.get('#conversation_query').type('Mi matrícula es A01027008')
         cy.get('#send_button').click()
         cy.wait(200)
-        cy.get('.foto-perfil').should('be.visible');
-        
+        cy.get('.foto-perfil').should('be.visible') 
+    })
+
+    it('matrícula incorrecta',()=>{
+        cy.get('.fa-bars').click()
+        cy.get(':nth-child(2) > .mina-bold-2').click()
+        cy.get('#conversation_query').type('Mi matrícula es A010101')
+        cy.get('#send_button').click()
+        cy.wait(200)
+        cy.contains('Lo siento').should('be.visible');
+    })
+
+    it('campo vacío',()=>{
+        cy.get('.fa-bars').click()
+        cy.get(':nth-child(2) > .mina-bold-2').click()
+        cy.get('#conversation_query').type('Mi matrícula es')
+        cy.get('#send_button').click()
+        cy.wait(200)
+        cy.contains('matrícula').should('be.visible');
     })
 })
