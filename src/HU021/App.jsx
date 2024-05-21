94c4d100ed5bb75e445070d038d8c18a267e7062
Nahomi Plata Ulate <nahomikawaii@gmail.com>
2024-05-20 23:02:49 -0600

    Ya se realiza la reservación método tradicional

--- src/HU021/App.jsx
+++ src/HU021/App.jsx
@@ -7,8 +7,6 @@ function App() {
   return (
     <div>
         <Header />
-      
-      
     </div>
   );
 }
--- src/HU021/Componentes/Header/Header.jsx
+++ src/HU021/Componentes/Header/Header.jsx
@@ -1,27 +1,23 @@
-import React from 'react'
-import './Header.css'
-import adperfil from '../../../assets/adperfil.png'
-
-
-
-
+import React from 'react';
+import './Header.css';
+import adperfil from '../../../assets/adperfil.png';
 
 const Header = () => {
   return (
-    <div className='Header container'>
+    <div className='Header'>
+      <div className='Header-content'>
         <div className='Header-text'>
-            <h1>INICIA SESIÓN</h1>
-            <img src={adperfil} className='botonnav'/>
-            <div className='text-container'>
+          <h1>INICIA SESIÓN</h1>
+          <img src={adperfil} className='botonnav' alt='adperfil' />
+          <div className='text-container'>
             <input type='text' placeholder='Usuario' />
             <input type='password' placeholder='Contraseña' />
-            <a href='/reservar'className='boton3'>Continuar</a>
-            </div>
-
-
+            <a href='/reservar' className='boton3'>Continuar</a>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
