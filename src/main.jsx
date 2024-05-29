c05de53c3381a0c2131bfd91d358ee1d8623b1fd
karengtz04 <102173278+karengtz04@users.noreply.github.com>
2024-05-28 21:55:11 -0600

    Cambios

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // import HU004 from "./HU004/HU004.jsx";
 // import HU008 from "./HU008/HU008.jsx";
-import HU009 from "./HU009/HU009";
+// import HU009 from "./HU009/HU009";
 // import HU0016_HU0017 from "./HU0016_HU0017/HU0016_HU0017";
 // import HU0019 from "./HU0019/HU0019";
 /*
@@ -20,7 +20,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         {/* <Route path="/" element={<HU0016_HU0017 />} /> */}
         {/* <Route path="/reservar" element={<HU004 />} /> */}
         {/* <Route path="/secciones" element={<HU008 />} /> */}
-        <Route path="/garage_valley" element={<HU009 />} />
+        {/* <Route path="/garage_valley" element={<HU009 />} /> */}
         {/* <Route path="/perfil" element={<HU0019 />} /> */}
      
         {/* <Route path="/admin2" element={<HU022 />} />  */}
