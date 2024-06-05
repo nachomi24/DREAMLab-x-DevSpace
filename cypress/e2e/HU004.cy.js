d30280801fa6f53f51eb8a8d52195a85807e906f
criostec <a00833125@tec.mx>
2024-06-05 00:10:28 -0600

    Modificacion de pruebas de cypress

--- cypress/e2e/HU004.cy.js
+++ cypress/e2e/HU004.cy.js
@@ -1,14 +1,15 @@
 //Para probar inicio de sesión módulo HU004
 describe('template spec', () => {
     beforeEach(()=>{
-        cy.visit('http://localhost:5173/')
+        cy.visit('https://green-ground-02320f30f.5.azurestaticapps.net/')
     })
 
     it('matrícula correcta',()=>{
         cy.get('.fa-bars').click()
         cy.get(':nth-child(2) > .mina-bold-2').click()
-        cy.get('#conversation_query').type('Mi matrícula es A01027008')
-        cy.get('#send_button').click()
+        cy.get(':nth-child(1) > input').type('A00833125');
+        cy.get(':nth-child(2) > input').type('cristina123');
+        cy.get('button').click()
         cy.wait(200)
         cy.get('.foto-perfil').should('be.visible') 
     })
@@ -16,18 +17,17 @@ describe('template spec', () => {
     it('matrícula incorrecta',()=>{
         cy.get('.fa-bars').click()
         cy.get(':nth-child(2) > .mina-bold-2').click()
-        cy.get('#conversation_query').type('Mi matrícula es A010101')
-        cy.get('#send_button').click()
+        cy.get(':nth-child(1) > input').type('A00833122');
+        cy.get(':nth-child(2) > input').type('cristina123');
+        cy.get('button').click()
         cy.wait(200)
-        cy.contains('Lo siento').should('be.visible');
+        cy.contains('Matrícula o contraseña incorrecta').should('be.visible');
     })
 
     it('campo vacío',()=>{
         cy.get('.fa-bars').click()
         cy.get(':nth-child(2) > .mina-bold-2').click()
-        cy.get('#conversation_query').type('Mi matrícula es')
-        cy.get('#send_button').click()
-        cy.wait(200)
-        cy.contains('matrícula').should('be.visible');
+        cy.get('button').click();
+            
     })
 })
--- cypress/e2e/HU016.cy.js
+++ cypress/e2e/HU016.cy.js
@@ -1,11 +1,11 @@
 //Para probar la visibilidad de los talleres
 describe('template spec', () => {
     beforeEach(()=>{
-        cy.visit('http://localhost:5173/')
+        cy.visit('https://green-ground-02320f30f.5.azurestaticapps.net/')
     })
 
     it('probar que se puede ver el tab de talleres',()=>{
-        cy.get('.contenedor-tarjetas').should('be.visible')
-        cy.get(':nth-child(1) > :nth-child(1) > .tarjeta-img-inside').should('be.visible')
+        cy.get('.contenedor-tarjetas016').should('be.visible')
+        cy.get(':nth-child(2) > :nth-child(1) > .tarjeta-img-inside016').should('be.visible')
     })
 })
